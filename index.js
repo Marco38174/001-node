1ece4d2829034c5eff85e86df42a0a47425426d8
Marco38174 <94290074+Marco38174@users.noreply.github.com>
2022-02-09 12:36:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require ('express')
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.get( '/', (req,res,netx) => {
+    res.render("index")
+})
+
+app.get( '/test', (req,res,netx) => {
+    
+    res.send("This is the test page")
+})
+
+app.get( '/paolo', (req,res,netx) => {
+    
+    res.json({
+        name: 'Paolo',
+        role: 'Teacher',
+        Classes: []
+    })
+
+})
+
+
+app.listen(3000)
